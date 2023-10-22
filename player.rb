fc98183abc15d788ab1d27192334aeafb80367b7
andrijana85 <topalovic.andrijana@yahoo.com>
2023-10-21 21:42:34 -0400

    Define Player file

--- player.rb
+++ player.rb
@@ -1,2 +1,21 @@
 class Player
+  attr_accessor :name, :lives
+
+  def initialize(name)
+    @name = name
+    @lives = 3
+  end
+
+  def lose_life
+    @lives -= 1
+  end
+
+  def is_alive?
+    @lives > 0
+  end
+
+  def to_s
+    "#{@name} has #{@lives} lives left"
+  end
+
 end
