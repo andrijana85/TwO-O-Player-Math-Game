1fe4e5312aee9cd5e95c91690b60b7822dce3d77
andrijana85 <topalovic.andrijana@yahoo.com>
2023-10-21 21:51:43 -0400

    Define Question file

--- question.rb
+++ question.rb
@@ -1,2 +1,13 @@
 class Question 
+  attr_accessor :input1, :input2
+
+  def generate_question
+    @input1 = rand(1..20)
+    @input2 = rand(1..20)
+    "What does #{@input1} plus #{@input2} equal?"
+  end
+
+  def check_answer?(answer)
+    answer == @input1 + @input2
+  end
 end
